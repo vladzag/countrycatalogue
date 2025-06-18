0fa802795b257dcf8a19cdce040e4f50a4bc020f
Загорский Владислав <vvzagorski@mts.ru>
2025-06-18 16:26:09 +0300

    Initial commit

---
+++ src/test/java/guru/qa/countrycatalogue/CountrycatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.qa.countrycatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrycatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
